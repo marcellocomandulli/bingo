8a753e95f137f69b3c6459d8a9655b6e1b99bc15
Marcello Comandulli <130607385+marcellocomandulli@users.noreply.github.com>
2025-08-28 20:25:48 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,48 +2,47 @@ const gameConsole = document.getElementById("game-console");
 const gameScreen = document.getElementById("game-screen");
 const gameControls = document.getElementById("game-controls");
 
-
-
 //CREATE BOARD'S NUMBERS AND THIER DIVs
 let randomArray = [];
 
 for (i = 1; i < 91; i++) {
-   randomArray.push(i);
-};
+  randomArray.push(i);
+}
 
 randomArray.forEach((num) => {
-   let div = document.createElement("div");
-   gameScreen.appendChild(div);
-   div.classList.add("number");
-   div.innerHTML = num;
+  let div = document.createElement("div");
+  gameScreen.appendChild(div);
+  div.classList.add("number");
+  div.innerHTML = num;
 });
-
 
 //SHUFFLE ARRAY'S NUMBERS
 function shuffle(array) {
-   let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
-   while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-      [array[currentIndex], array[randomIndex]] = [
-         array[randomIndex], array[currentIndex]];
-   }
-   return array;
-};
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
 shuffle(randomArray);
-
 
 //CREATE GAME CONTROLS --> BUTTONS, NUMBERS ALREADY EXTRACTED AND
 //THE ACTUAL NUMBER (AKA THE NUMBER JUST EXTRACTED)
 const plusBtn = document.createElement("button");
 plusBtn.setAttribute("id", "plus");
-plusBtn.innerHTML = "Draw!";
+plusBtn.innerHTML = "+";
 
 const minusBtn = document.createElement("button");
 minusBtn.setAttribute("id", "minus");
-minusBtn.innerHTML = "Back";
+minusBtn.innerHTML = "-";
 
 const resetBtn = document.createElement("button");
 resetBtn.setAttribute("id", "reset");
@@ -55,7 +54,6 @@ const counter = document.createElement("h2");
 let count = 0;
 counter.innerHTML = count;
 
-
 gameControls.appendChild(extracted);
 gameControls.appendChild(counter);
 gameControls.appendChild(plusBtn);
@@ -64,9 +62,8 @@ gameControls.appendChild(minusBtn);
 
 //RESET FUNCTION
 resetBtn.addEventListener("click", function () {
-   location.reload();
+  location.reload();
 });
-
 
 //COUNTER NUMBERS EXTRACTED,
 //ACTUAL NUMBER EXTRACTED,
@@ -75,35 +72,33 @@ let actualNumber = document.getElementById("act-num");
 let lastNumbers = document.getElementById("last-num");
 let j = 0;
 
-
 gameControls.addEventListener("click", function (e) {
+  let fourNums = [];
 
-   let fourNums = [];
+  if (e.target.id === "plus" && count >= 0 && count < 90) {
+    count++;
+    counter.innerHTML = count; //COUNTER OF NUMBERS EXTRACTED
 
-   if (e.target.id === "plus" && count >= 0 && count < 90) {
-      count++;
-      counter.innerHTML = count; //COUNTER OF NUMBERS EXTRACTED
+    actualNumber.innerHTML = randomArray[j]; //SHOW THE NUMBER JUST EXTRACTED
+    j++;
 
-      actualNumber.innerHTML = randomArray[j]; //SHOW THE NUMBER JUST EXTRACTED
-      j++;
+    for (n = count - 4; n < count; n++) {
+      //SHOW THE LAST 4 NUMBERS EXTRACTED
+      fourNums.push(randomArray[n]);
+    }
 
-      for (n = count - 4; n < count; n++) {    //SHOW THE LAST 4 NUMBERS EXTRACTED
-         fourNums.push(randomArray[n]);
-      };
+    lastNumbers.innerHTML = fourNums.join(" ");
+  } else if (e.target.id === "minus" && count > 1 && count < 91) {
+    count--;
+    counter.innerHTML = count;
 
-      lastNumbers.innerHTML = fourNums.join(" ");
+    actualNumber.innerHTML = randomArray[j - 2];
+    j--;
 
-   } else if (e.target.id === "minus" && count > 1 && count < 91) {
-      count--;
-      counter.innerHTML = count;
+    for (n = count - 4; n < count; n++) {
+      fourNums.push(randomArray[n]);
+    }
 
-      actualNumber.innerHTML = randomArray[j - 2];
-      j--;
-
-      for (n = count - 4; n < count; n++) {
-         fourNums.push(randomArray[n]);
-      };
-
-      lastNumbers.innerHTML = fourNums.join(" ");
-   }
+    lastNumbers.innerHTML = fourNums.join(" ");
+  }
 });
